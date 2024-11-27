fe151d08c3f7ecfe73beca5e6c3d7344a3694eac
C0rel <vadimskedov@gmail.com>
2024-11-28 00:32:40 +0300

    update master

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p
+        <p>
+        Бобырь Павел Евгеньевич
         </p>
         <a
           className="App-link"
@@ -14,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Лабораторная 5
+          Лабораторная 4
         </a>
       </header>
     </div>
